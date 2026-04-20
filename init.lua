6c680b5d22679d838f4d09422389178d60126774
Martin Otten <martinotten@web.de>
2026-04-20 09:58:45 +0200

    Hinzufügen von weiteren LSP

--- init.lua
+++ init.lua
@@ -600,16 +600,21 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        omnisharp = {},
+        kotlin_language_server = {},
+        bashls = {},
+        julials = {},
+        haskellls = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
 
         stylua = {}, -- Used to format Lua code
 
